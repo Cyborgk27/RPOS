af324f9357dbbe9d16e7505ead9a83c294261856
CyborgK27 <slenderstalin@hotmail.com>
2024-06-17 21:49:43 -0500

    add entities MenuForDay

---
+++ RPOS.Domain/Entities/MenuForDay.cs
@@ -0,0 +1,13 @@
+﻿using RPOS.Domain.Domain;
+
+namespace RPOS.Domain.Entities
+{
+    public class MenuForDay : BaseEntity
+    {
+        public MenuForDay() 
+        {
+            Products = new List<Product>();
+        }
+        public ICollection<Product> Products { get; set; } = null!;
+    }
+}
